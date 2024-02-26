fef6f022e3077cce59626bcd1b6ecc1903bffdb6
Faisal <faisal.akhlaq@aol.com>
2024-02-26 23:40:48 +0100

    Added django-environment in the production requirements to read env file.

--- settings/production.py
+++ settings/production.py
@@ -1,16 +1,24 @@
-import os
+import environ
 
 from .base import *
 
-DEBUG = False
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+DEBUG = env('DEBUG')
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DATABASE_NAME"),
-        "USER": os.environ.get("DATABASE_USER"),
-        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        "HOST": os.environ.get("DATABASE_HOST_MYSQL"),
-        "PORT": os.environ.get("DATABASE_PORT_MYSQL"),
+        "NAME": env("DATABASE_NAME_MYSQL"),
+        "USER": env("DATABASE_USER_MYSQL"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST_MYSQL"),
+        "PORT": env("DATABASE_PORT_MYSQL"),
     }
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
