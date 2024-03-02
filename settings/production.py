465697e1695794a455c1a67307fa6cede23bf99f
Faisal <faisal.akhlaq@aol.com>
2024-03-02 12:24:45 +0100

    Minor. Adding rest_framework setting in production.

--- settings/production.py
+++ settings/production.py
@@ -32,3 +32,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 STATIC_ROOT = BASE_DIR / "static"  # type: ignore
+
+REST_FRAMEWORK = {
+    "NON_FIELD_ERRORS_KEY": "error",
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
