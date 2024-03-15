5da70feb5995cf95d5ae9df053e20f059134c9f3
Faisal <faisal.akhlaq@aol.com>
2024-03-15 23:51:46 +0100

    Create city table.

--- apps/core/models/__init__.py
+++ apps/core/models/__init__.py
@@ -2,9 +2,11 @@ from .address import Address
 from .genre import Genre
 from .status import Status
 from .timetracking import TimeTracking
+from .city import City
 
 __all__ = [
     "Address",
+    "City",
     "Genre",
     "Status",
     "TimeTracking",
---
+++ apps/core/models/city.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.name}, {self.country}"
